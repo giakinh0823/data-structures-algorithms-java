f6376c17246890235173da368ab0f76f5d4f16a0
giakinh0823 <giakinh2000@gmail.com>
2024-05-06 23:21:37 +0700

    add queue and stack

--- src/main/java/gurucoding/org/stack/Stack.java
+++ src/main/java/gurucoding/org/stack/Stack.java
@@ -30,6 +30,9 @@ public class Stack {
     }
 
     public Node pop() {
+        if (height == 0) {
+            return null;
+        }
         Node temp = top;
         top = top.next;
         temp.next = null;
